09bfb521c2b92a31c11637639a111b6d77c59315
Blur <danielthesmartypants2017@gmail.com>
2023-03-22 01:26:36 -0500

    bye

--- src/Lua/2-ZECore/2-Console/con_cmds.lua
+++
@@ -1,8 +0,0 @@
-COM_AddCommand("ze_spectate", function(player)
-	 player.spectator = 1
-	 player.ctfteam = 0
-end, 1)
-
-COM_AddCommand("unlockprop", function(player)
-     player.propspawn = $+10
-end, 1)
