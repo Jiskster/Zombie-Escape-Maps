1066db7b1354655fbb883796a789db93c38981d5
Blur <danielthesmartypants2017@gmail.com>
2023-05-21 01:19:50 -0500

    memoryofwrath take 2

---
+++ src/Lua/MapT3.lua
@@ -0,0 +1,10 @@
+--Sets gametime to 4:16 and wait time to 0:10 from the level loading
+
+
+-- i dont get this below 5/21/23 Jisk
+local function realtime()
+	COM_BufInsertText(server, "ze_wait 10")
+	COM_BufInsertText(server, "ze_survtime 256")
+end
+
+addHook("LinedefExecute", realtime, "RSTIME")
